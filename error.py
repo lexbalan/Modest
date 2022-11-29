4e77fb76e1d00daa8eb2078b3cbf4c5c89429b49
Alex Balan <alexbalan@Alexs-MacBook-Air.local>
2022-11-29 21:25:11 +0200

    space

--- error.py
+++ error.py
@@ -1,29 +1,19 @@
 
 
-
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKCYAN = '\033[96m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
-
-
 warncnt = 0
 errcnt = 0
 
+
 def getline(file, lineno):
-  #print("FILE=%s LINE=%s" % (file, lineno))
   f = open(file, 'r')
   lin = f.read().split("\n")[lineno - 1]
   f.close()
   return lin
 
+
 def highlight(pos):
   print(" " * pos, end=''); print('\033[92m' + '^' + '\033[0m')
+
 
 def warning(s, ti):
   global warncnt
