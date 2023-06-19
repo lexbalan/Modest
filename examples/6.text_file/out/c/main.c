b06e271f0ffc4e9499e79701e65ca78904a413c6
Alex Balan <alexbalan@Alexs-MacBook-Air.local>
2023-06-19 19:52:07 +0300

    add examples/6.text_file

---
+++ examples/6.text_file/out/c/main.c
@@ -0,0 +1,43 @@
+
+#include <stdint.h>
+
+#include <stdio.h>
+
+#define filename  "file.txt"
+
+void write_example() {
+	printf("run write_example\n");
+	FILE * const fp = fopen(filename, "w");
+	if(fp == NULL) {
+		printf("error: cannot create file '%s'", filename);
+		return;
+	}
+	fprintf(fp, "some text.\n");
+	fclose(fp);
+}
+
+void read_example() {
+	printf("run read_example\n");
+	FILE * const fp = fopen(filename, "r");
+	if(fp == NULL) {
+		printf("error: cannot open file '%s'", filename);
+		return;
+	}
+	printf("file '%s' contains: ", filename);
+	while(1) {
+		const int32_t ch = fgetc(fp);
+		if(ch == EOF) {
+			break;
+		}
+		putchar(ch);
+	}
+	fclose(fp);
+}
+
+int32_t main() {
+	printf("text_file example\n");
+	write_example();
+	read_example();
+	return 0;
+}
+
