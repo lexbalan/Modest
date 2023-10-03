bb3567cdb4023fff2539a437d550a45cbefe7996
Alex Balan <alexbalan@Alexs-MacBook-Air.local>
2023-10-03 17:44:58 +0300

    + test/const

---
+++ test/3.const/out/c/main.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+// test/2.func/main.cm
+
+#define genericIntConst  42
+#define int32Const  ((int32_t)genericIntConst)
+
+#define genericStringConst  "Hello!"
+#define genericString8Const  genericStringConst
+#define genericString16Const  u##genericStringConst
+#define genericString32Const  U##genericStringConst
+
+
+// define function main
+int main(void)
+{
+    printf("test const\n");
+
+    printf("genericIntConst = %d\n", genericIntConst);
+    printf("int32Const = %d\n", int32Const);
+
+    printf("genericStringConst = %s\n", genericStringConst);
+    printf("genericString8Const = %s\n", genericString8Const);
+
+    return 0;
+}
+
