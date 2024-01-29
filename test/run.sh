40a7c5b3e026e1bf16051cae9796c37536800f9b
Alex Balan <outsiders.b@gmail.com>
2024-01-29 20:01:49 +0200

    1

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,4 @@
+cd 0.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
 cd 1.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
 cd 2.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
 cd 3.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
