e80f4374c7ea0b76f4ec6fd2ddcbe56ceca41853
Alex Balan <outsiders.b@gmail.com>
2024-02-02 22:38:42 +0200

    1

--- test/1.hello_world/out/c/main.c
+++ test/1.hello_world/out/c/main.c
@@ -6,20 +6,10 @@
 
 #include <stdio.h>
 
-//@property("type.c_alias", "Res")
-//@property("id.str", "Res")
-typedef volatile struct {
-    uint32_t v;
-} Rec;
-
-static volatile bool x;
-
-static volatile Rec r;
 
 int main()
 {
     printf("%s", "Hello World!\n");
-
     return 0;
 }
 
