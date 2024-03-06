9767153c2c731911e84374481f29c4db0f549c6d
Alex Balan <outsiders.b@gmail.com>
2024-03-06 22:23:48 +0200

    1

--- examples/build_all.sh
+++ examples/build_all.sh
@@ -7,7 +7,9 @@ cd 6.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
 cd 7.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
 cd 8.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
 cd 9.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
-cd 10.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
-cd stmt_if && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
-cd stmt_while && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
+cd 10.* && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
+cd stmt_if && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
+cd stmt_while && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. &&\
+cd demo1 && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
+cd bubble_sort && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
 
