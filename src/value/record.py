b6629c801be3d45d9f7532a295ad1b7e2258535e
Alex Balan <outsiders.b@gmail.com>
2024-04-19 23:00:27 +0300

    1

--- src/value/record.py
+++ src/value/record.py
@@ -41,9 +41,7 @@ def value_record_create(initializers=[], ti=None):
 
 
 
-
-
-def cons_initializers(initializers, rec_type, method):
+"""def cons_initializers(initializers, rec_type, method):
     from value.cons import value_cons, value_cons_implicit
     import copy
 
@@ -64,59 +62,10 @@ def cons_initializers(initializers, rec_type, method):
 
     return initializers2
 
+"""
 
 
-#def value_cons_record_immediate(v, t, ti, method):
-#    info("value_cons_record_immediate", ti)
-
-#    return value_cons_record_from_generic_record(v, t, ti, method)
-
-    """# TODO
-    #assets = []
-    from value.cons import value_cons, value_cons_implicit
-    import copy
-
-    #initializers = cons_initializers(v['asset'], t, method)
-
-    initializers = []
-    for initializer in v['asset']:
-        field_id = initializer['id']
-        init_value = initializer['value']
-        # получаем поле с таким именем
-        f = get_item_with_id(t['fields'], field_id['str'])
-
-        # приводим инициализатор к типу поля
-        #iv = value_cons_implicit(init_value, f['type'])
-        iv = value_cons(init_value, f['type'], init_value['expr_ti'], method)
-
-        info("cons to", v['expr_ti'])
-        type.type_print(f['type'])
-
-        ni = copy.copy(initializer)
-        ni['value'] = iv
-        initializers.append(ni)
-
-    nv = value_cons_immediate(v, t, ti)
-    nv['asset'] = initializers
-
-    #from hlir.type import type_print
-    #type_print(nv['type'])
-
-    return nv"""
-
-
-def value_cons_record_from_generic_record(v, t, ti, method):
-    #if v['kind'] == 'const':
-        # TODO: тут нужно проверить чтобы при implicit методе
-        # все поля присутствовали (!)
-
-        #for field in t['fields']:
-        #    print(field['id']['str'])
-
-     #   return value_cons_node(v, t, ti=ti)
-
-
-    #info("value_cons_record_from_generic_record", ti)
+def _doitems(v, t, ti, method):
 
     items = []
     if len(v['type']['fields']) > 0:
@@ -166,13 +115,16 @@ def value_cons_record_from_generic_record(v, t, ti, method):
             p = hlir_initializer(field['id'], item_value2, ti=ti, nl=nl)
             items.append(p)
 
+    return items
+
+
+
+def value_cons_record_from_generic_record(v, t, ti, method):
+    items = _doitems(v, t, ti, method)
 
     nv = value_terminal(t, items, ti)
 
-    #items = cons_initializers(v['asset'], t, method)
-    #nv['asset'] = initializers
-
-    if 'nl_end' in v: #FIXIT: nl_end должен быть взде!
+    if 'nl_end' in v: #FIXIT: nl_end должен быть везде!
         nv['nl_end'] = v['nl_end']
 
     if value_is_immediate(v):
@@ -191,8 +143,9 @@ def value_cons_record_from_generic_record(v, t, ti, method):
 
 
 
-def value_cons_record_from_record(v, t, ti, method):
-    return value_cons_node(v, t, ti=ti)
+def do_cons_record(v, t, ti, method):
+    nv = value_cons_node(v, t, ti=ti)
+    return nv
 
 
 
@@ -202,30 +155,38 @@ def value_cons_record(v, t, ti, method):
     #if value_is_immediate(v):
     #    return value_cons_record_immediate(v, t, ti, method)
 
-    if type.type_is_record(from_type):
-        # GenericRecord -> Record  (implicit)
-        if type.type_is_generic(from_type):
-            return value_cons_record_from_generic_record(v, t, ti, method)
 
-        if method != 'explicit':
-            info("cannot implicitly cons Record value", ti)
-            return None
+    if not type.type_is_record(from_type):
+        return None
+
+    #
+    # Implicit cons
+    #
+
+    # GenericRecord -> Record (implicit)
+    if type.type_is_generic(from_type):
+        return value_cons_record_from_generic_record(v, t, ti, method)
 
 
-        # check if all fields in from_type present in t
-        # and their types are equal (!)
-        for field in from_type['fields']:
-            field2 = record_field_get(t, field['id']['str'])
-            if field2 == None:
-                return None  # if no field with that name
-            if not type.type_eq(field['type'], field2['type']):
-                return None  # if field type not equal
+    if method != 'explicit':
+        info("cannot implicitly cons Record value", ti)
+        return None
 
-            #print(field['id']['str'])
+    #
+    # Explicit cons
+    #
 
-        # Record -> Record (explicit)
-        return value_cons_record_from_record(v, t, ti, method)
+    # check if all fields in from_type present in t
+    # and their types are equal (!)
+    for field in from_type['fields']:
+        field2 = record_field_get(t, field['id']['str'])
+        if field2 == None:
+            return None  # if no field with that name
+        if not type.type_eq(field['type'], field2['type']):
+            return None  # if field type not equal
 
-    return None
+
+    # Record -> Record (explicit)
+    return do_cons_record(v, t, ti, method)
 
 
