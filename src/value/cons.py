d8a76625ca9860d8b2695b124eeadfc5e48e4acd
Alex Balan <outsiders.b@gmail.com>
2024-05-01 22:27:21 +0300

    1

--- src/value/cons.py
+++ src/value/cons.py
@@ -37,6 +37,7 @@ def _value_cons(t, v, method, ti):
     elif type.type_is_pointer(t): constructor = value_cons_pointer
     elif type.type_is_unit(t): constructor = value_cons_unit
     elif type.type_is_bad(t): constructor = value_cons_bad
+    else: assert False, "unknown type kind '%s'" % t['kind']
 
     if constructor == None:
         return None
@@ -50,20 +51,16 @@ def _value_cons(t, v, method, ti):
 
 
 
-
-def _implicit_cons_if_possible(t, v, ti):
-    c = _value_cons(t, v, 'implicit', ti)
-
-    if c == None:
-        return v
-
-    return c
+def _try_to_implicit_cons(t, v, ti):
+    nv = _value_cons(t, v, 'implicit', ti)
+    return nv if (nv != None) else v
 
 
 
 def value_cons_implicit(t, v):
     assert(t['isa'] == 'type')
     assert(v['isa'] == 'value')
+
     if value_is_bad(v) or type.type_is_bad(t):
         return value_bad(v['expr_ti'])
 
@@ -86,12 +83,11 @@ def value_cons_implicit(t, v):
 
     # for structural type system support
     if type.type_is_record(t) and type.type_is_record(from_type):
-
         if type.type_is_generic(from_type):
-            return _implicit_cons_if_possible(t, v, ti)
+            return _try_to_implicit_cons(t, v, ti)
 
         if not type.type_eq_record(t, from_type, opt=[], nominative=True):
-            return value_cons_node(t, v, 'implicit', ti=ti)  # value_cast!
+            return value_cons_node(t, v, 'implicit', ti=ti)  # value_cons_node!
 
     # for structural type system support
     if type.type_is_pointer_to_record(t):
@@ -101,29 +97,20 @@ def value_cons_implicit(t, v):
                 return v
             elif type.type_eq_record(from_type['to'], t['to'], opt=[]):
                 # если равны но не номенативно - для C & LLVM нужно привдение
-                return value_cons_node(t, v, 'implicit', ti=ti)  # value_cast!
+                return value_cons_node(t, v, 'implicit', ti=ti)  # value_cons_node!
 
 
     if type.type_eq(from_type, t):
         return v
 
     if type.type_is_generic(from_type):
-        return _implicit_cons_if_possible(t, v, ti)
+        return _try_to_implicit_cons(t, v, ti)
 
-    # cons Pointer from:
     if type.type_is_pointer(t):
-        #return _implicit_cons_if_possible(t, v, ti) #?
-        return value_cons_pointer(t, v, 'implicit', ti)
-
+        return _try_to_implicit_cons(t, v, ti) #?
 
     return v
 
-
-
-def value_cons_implicit_check(t, v):
-    nv = value_cons_implicit(t, v)
-    type.check(nv['type'], t, v['expr_ti'])
-    return nv
 
 
 def value_cons_explicit(t, v, ti):
@@ -134,13 +121,13 @@ def value_cons_explicit(t, v, ti):
         warning("explicit cast to the same type", ti)
         return v
 
-    y = _value_cons(t, v, 'explicit', ti)
+    nv = _value_cons(t, v, 'explicit', ti)
 
-    if y == None:
+    if nv == None:
         error("cannot construct value", ti)
         return value_bad(v['expr_ti'])
 
-    return y
+    return nv
 
 
 
@@ -157,19 +144,27 @@ def value_cons_default(x, ti):
         t = typeSysInt
         if not type.type_is_signed(from_type):
             t = typeSysNat
-        return value_cons_integer(t, x, 'implicit', ti)
+        #return value_cons_integer(t, x, 'implicit', ti)
+        return _try_to_implicit_cons(t, x, ti)
 
     elif type.type_is_string(from_type):
         return cons_ptr_to_str_from_string(typeSysStr, x, ti)
 
     elif type.type_is_float(from_type):
-        return value_cons_float(typeSysFloat, x, 'implicit', ti)
+        return _try_to_implicit_cons(typeSysFloat, x, ti)
 
     elif type.type_is_char(from_type):
-        return value_cons_char(typeSysChar, x, 'implicit', ti)
+        return _try_to_implicit_cons(typeSysChar, x, ti)
 
     from error import fatal
     fatal("unimplemented value_cons_default case")
+
+
+
+def value_cons_implicit_check(t, v):
+    nv = value_cons_implicit(t, v)
+    type.check(nv['type'], t, v['expr_ti'])
+    return nv
 
 
 
