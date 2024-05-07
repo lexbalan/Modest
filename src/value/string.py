c736948cea541e7facd150cf3d18a80d59a905cf
Alex Balan <outsiders.b@gmail.com>
2024-05-07 21:43:11 +0300

    1

--- src/value/string.py
+++ src/value/string.py
@@ -4,14 +4,9 @@ from util import nbits_for_num
 from hlir.type import hlir_type_string
 from .value import value_bin
 
+
 def value_string_create(string, ti=None):
 	#info("value_string_create %d" % length, ti)
-	"""chars = []
-	for char in string:
-		cc = ord(char)
-		char_value = value_char_create(cc, _type=None, ti=ti)
-		chars.append(char_value)"""
-
 	max_char_width = 8
 	for c in string:
 		cc = ord(c)
@@ -28,7 +23,6 @@ def value_string_create(string, ti=None):
 def value_string_concat(l, r, ti):
 	#info("value_array_concat", ti)
 	newstring = l['asset'] + r['asset']
-	#return value_string_create(newstring, ti)
 	max_char_width = max(l['type']['width'], r['type']['width'])
 	string_type = hlir_type_string(max_char_width, ti)
 	nv = value_bin('concat_string', l, r, string_type, ti=ti)
