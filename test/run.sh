b16ab7f0e211d6be9f38dd8c75e6baf7cbbb034d
Alex Balan <outsiders.b@gmail.com>
2024-06-08 21:07:45 +0300

    1

--- test/run.sh
+++ test/run.sh
@@ -35,5 +35,6 @@ cd slices && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
 #cd crc32 && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
 cd float && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
 cd free_pointer && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
+cd unistd && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd .. && \
 cd generic && echo "\nBUILD:" "$PWD" && make && make C && make CM && cd ..
 
