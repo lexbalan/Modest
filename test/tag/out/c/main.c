863e97f85218b9459b29600357187bc97efa798e
Alex Balan <outsiders.b@gmail.com>
2024-06-27 10:03:12 +0300

    1

--- test/tag/out/c/main.c
+++ test/tag/out/c/main.c
@@ -8,31 +8,15 @@
 
 
 
-//@feature("unsafe")
 
 
-typedef struct {
-    int32_t x;
-    int32_t y;
-} Point;
 
 int main()
 {
-    printf("tag test");
+	printf("tag test");
 
-    int32_t *const p = 0;
-    printf("*p = %i\n", *p);
+	//var s : Tag = #justSymbol
 
-    int32_t a;
-
-    int32_t arr[10];
-    memcpy(&arr, &(int32_t[10]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 40);
-
-    Point p0;
-    p0 = (Point){};
-
-    //var s : Tag = #justSymbol
-
-    return 0;
+	return 0;
 }
 
