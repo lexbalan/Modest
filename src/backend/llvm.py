4b8f96f53e8620ab951b9ccaa4d98d8960c599c5
Alex Balan <outsiders.b@gmail.com>
2024-08-19 09:49:56 +0300

    1

--- src/backend/llvm.py
+++ src/backend/llvm.py
@@ -2288,7 +2288,6 @@ def print_module(m):
 		out("\n; ------------------------------------------------")
 		out("\n; declarations from: %s" % (imported_module_id))
 		out("\n; ------------------------------------------------")
-		print(len(imp['export_defs']))
 		een(imp['export_defs'])
 		out("\n\n")
 
