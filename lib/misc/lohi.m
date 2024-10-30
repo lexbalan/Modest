37f7b8b53ece87ee9c8afa8173c44546e56a53fa
Alex Balan <outsiders.b@gmail.com>
2024-10-30 21:31:30 +0200

    1

--- lib/misc/lohi.m
+++ lib/misc/lohi.m
@@ -1,89 +1,42 @@
 // lohi.m
 
-//
-// Unsigned
-//
 
-public func lo_nat16(x: Nat16) -> Nat8 {
-	return Nat8 (x and 0xFF)
+public func lo16(x: Word16) -> Word8 {
+	return Word8 (x and 0xFF)
 }
 
 
-public func hi_nat16(x: Nat16) -> Nat8 {
-	return Nat16 (x >> 8)
+public func hi16(x: Word16) -> Word8 {
+	return Word16 (x >> 8)
 }
 
 
-public func lo_nat32(x: Nat32) -> Nat16 {
-	return Nat16 (x and 0xFFFF)
+public func lo32(x: Word32) -> Word16 {
+	return Word16 (x and 0xFFFF)
 }
 
 
-public func hi_nat32(x: Nat32) -> Nat16 {
-	return Nat16 (x >> 16)
+public func hi32(x: Word32) -> Word16 {
+	return Word16 (x >> 16)
 }
 
 
-public func lo_nat64(x: Nat64) -> Nat32 {
-	return Nat32 (x and 0xFFFFFFFF)
+public func lo64(x: Word64) -> Word32 {
+	return Word32 (x and 0xFFFFFFFF)
 }
 
 
-public func hi_nat64(x: Nat64) -> Nat32 {
-	return Nat32 (x >> 32)
+public func hi64(x: Word64) -> Word32 {
+	return Word32 (x >> 32)
 }
 
 
-public func lo_nat128(x: Nat128) -> Nat64 {
-	return Nat64 (x and 0xFFFFFFFFFFFFFFFF)
+public func lo128(x: Word128) -> Word64 {
+	return Word64 (x and 0xFFFFFFFFFFFFFFFF)
 }
 
 
-public func hi_nat128(x: Nat128) -> Nat64 {
-	return Nat64 (x >> 64)
-}
-
-
-//
-// Signed
-//
-
-public func lo_int16(x: Int16) -> Int8 {
-	return Int8 (x and 0xFF)
-}
-
-
-public func hi_int16(x: Int16) -> Int8 {
-	return Int16 (x >> 8)
-}
-
-
-public func lo_int32(x: Int32) -> Int16 {
-	return Int16 (x and 0xFFFF)
-}
-
-
-public func hi_int32(x: Int32) -> Int16 {
-	return Int16 (x >> 16)
-}
-
-
-public func lo_int64(x: Int64) -> Int32 {
-	return Int32 (x and 0xFFFFFFFF)
-}
-
-
-public func hi_int64(x: Int64) -> Int32 {
-	return Int32 (x >> 32)
-}
-
-
-public func lo_int128(x: Int128) -> Int64 {
-	return Int64 (x and 0xFFFFFFFFFFFFFFFF)
-}
-
-
-public func hi_int128(x: Int128) -> Int64 {
-	return Int64 (x >> 64)
+public func hi128(x: Word128) -> Word64 {
+	return Word64 (x >> 64)
 }
 
