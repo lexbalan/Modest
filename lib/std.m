2d220c5889b3ab434723bb67c9b7320232e1d6f6
Alex Balan <outsiders.b@gmail.com>
2024-12-14 11:51:30 +0200

    1

--- lib/std.m
+++ lib/std.m
@@ -2,13 +2,13 @@
 
 $pragma do_not_include
 
-$if (__target.intWidth == 32)
+//$if (__target.intWidth == 32)
 type Int Int32
 type Nat Nat32
-$elseif (__target.intWidth == 64)
-type Int Int64
-type Nat Nat64
-$else
-@error ("bad __target.intWidth")
-$endif
+//$elseif (__target.intWidth == 64)
+//type Int Int64
+//type Nat Nat64
+//$else
+//@error ("bad __target.intWidth")
+//$endif
 
