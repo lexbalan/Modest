e04276fc71a1b42587f73f830746770ebbea46c6
Alex Balan <outsiders.b@gmail.com>
2024-12-23 22:01:08 +0200

    1

--- test/composite/src/main.m
+++ test/composite/src/main.m
@@ -10,41 +10,41 @@ type RGB24 record {
 	blue: Nat8
 }
 
-var rgb0 = [2]RGB24 [{red=200, green=0, blue=0}, {red=200, green=0, blue=0}]
+var rgb0 = [2]RGB24 [{red = 200, green = 0, blue = 0}, {red = 200, green = 0, blue = 0}]
 
 type AnimationPoint record {
 	color: RGB24
 	time: Nat32
 }
 
-var ap = AnimationPoint {color={red=200, green=0, blue=0}, time=3000}
+var ap = AnimationPoint {color = {red = 200, green = 0, blue = 0}, time = 3000}
 
 
 var animation0_points = []AnimationPoint [
 	// testing select_common_record_type()
-	{color={red=200, green=0, blue=0}, time=3}
-	{color={red=0, green=200, blue=0}, time=30}
-	{color={red=100, green=100, blue=0}, time=300}
-	{color={red=254, green=254, blue=0}, time=20}
-	{color={red=0, green=0, blue=255}, time=3000}
+	{color = {red = 200, green = 0, blue = 0}, time = 3}
+	{color = {red = 0, green = 200, blue = 0}, time = 30}
+	{color = {red = 100, green = 100, blue = 0}, time = 300}
+	{color = {red = 254, green = 254, blue = 0}, time = 20}
+	{color = {red = 0, green = 0, blue = 255}, time = 3000}
 ]
 
 var animation1_points = []AnimationPoint [
 	// testing select_common_record_type()
-	{color={red=200, green=0, blue=0}, time=3}
-	{color={red=0, green=200, blue=0}, time=30}
-	{color={red=100, green=100, blue=0}, time=300}
-	{color={red=254, green=254, blue=0}, time=20}
-	{color={red=0, green=0, blue=255}, time=3000}
+	{color = {red = 200, green = 0, blue = 0}, time = 3}
+	{color = {red = 0, green = 200, blue = 0}, time = 30}
+	{color = {red = 100, green = 100, blue = 0}, time = 300}
+	{color = {red = 254, green = 254, blue = 0}, time = 20}
+	{color = {red = 0, green = 0, blue = 255}, time = 3000}
 ]
 
 var animation2_points = []AnimationPoint [
 	// testing select_common_record_type()
-	{color={red=200, green=0, blue=0}, time=3}
-	{color={red=0, green=200, blue=0}, time=30}
-	{color={red=100, green=100, blue=0}, time=300}
-	{color={red=255, green=254, blue=0}, time=20}
-	{color={red=0, green=0, blue=255}, time=3000}
+	{color = {red = 200, green = 0, blue = 0}, time = 3}
+	{color = {red = 0, green = 200, blue = 0}, time = 30}
+	{color = {red = 100, green = 100, blue = 0}, time = 300}
+	{color = {red = 255, green = 254, blue = 0}, time = 20}
+	{color = {red = 0, green = 0, blue = 255}, time = 3000}
 ]
 
 
