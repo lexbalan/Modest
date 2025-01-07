5ae98e8041ac3805345d3921d6aef3b963e1b131
Alex Balan <outsiders.b@gmail.com>
2025-01-07 19:04:22 +0200

    1

--- src/hlir/hlir.py
+++ src/hlir/hlir.py
@@ -1,57 +1,5 @@
 
-from .entity import Entity
-
+from .misc import *
 from .stmt import *
 from .value import *
-
-
-class Id(Entity):
-	def __init__(self, x=None):
-		super().__init__(None)
-		self.str = None
-		#self.ti = None
-
-		if x != None:
-			self.str = x['str']
-			self.ti = x['ti']
-
-		self.need_decoration = False
-
-		self.c = None
-		self.llvm = None
-		self.cm = None
-
-
-	def fromStr(self, x):
-		self.str = x
-		return self
-
-
-
-class Field(Entity):
-	def __init__(self, id, type, ti=None):
-		super().__init__(ti)
-		self.id = id
-		self.type = type
-		self.field_no = 0
-		self.offset = 0
-		self.access_level = 'private'
-		self.att = []
-		self.nl = 0
-		self.ti = ti
-		self.comments = None
-
-
-
-class Initializer(Entity):
-	def __init__(self, id, value, ti=None, nl=0):
-		super().__init__(ti)
-		self.id = id
-		self.value = value
-		self.ti = ti
-		self.nl = nl
-		self.att = []
-
-
-
 
