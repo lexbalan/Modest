113d696b8a5e9aa6fa3306827595a1775213dc5e
Alex Balan <outsiders.b@gmail.com>
2025-05-01 23:11:07 +0300

    1

---
+++ tests/lex/out/c/main.h
@@ -0,0 +1,20 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+
+
+int main();
+
+#endif /* MAIN_H */
---
+++ tests/lex/out/cm/main.m
@@ -0,0 +1,123 @@
+include "stdio"
+include "ctype"
+include "libc"
+
+
+
+const filename = *Str8 "file.txt"
+
+const tokenId = 1
+const tokenNum = 2
+const tokenSym = 3
+
+type Lexer record {
+	fd: *File
+	cc: [2]Char8
+	nback: Nat8
+
+	ttype: Nat16
+	token: [256]Char8
+	toklen: Nat16
+}
+
+var lex: Lexer
+
+
+func init(object: *Lexer) -> Unit {
+	object.toklen = 0
+}
+
+
+
+func is_alpha(c: Char8) -> Bool {
+	return isalpha(Int Word32 c)
+}
+
+func is_digit(c: Char8) -> Bool {
+	return isdigit(Int Word32 c)
+}
+
+
+func getcc(lex: *Lexer) -> Char8 {
+	let c: Int = fgetc(lex.fd)
+	return Char8 Word8 Word32 c
+}
+
+
+func putcc(lex: *Lexer, c: Char8) -> Unit {
+	ungetc(Int Word32 Word8 c, lex.fd)
+}
+
+const eof = Char8 Word8 0xFF
+
+func gettok(lex: *Lexer) -> Bool {
+	lex.toklen = 0
+	var c: Char8 = getcc(lex)
+
+	// skip blanks
+	while c == " " or c == "\t" {
+		c = getcc(lex)
+	}
+
+	if is_alpha(c) or c == "_" {
+		lex.ttype = tokenId
+		while is_alpha(c) or is_digit(c) or c == "_" {
+			lex.token[lex.toklen] = c
+			lex.toklen = lex.toklen + 1
+			c = getcc(lex)
+		}
+	} else if is_digit(c) {
+		lex.ttype = tokenNum
+		while is_alpha(c) or is_digit(c) or c == "_" {
+			lex.token[lex.toklen] = c
+			lex.toklen = lex.toklen + 1
+			c = getcc(lex)
+		}
+	} else {
+		lex.ttype = tokenSym
+
+		if c == "-" {
+			lex.token[lex.toklen] = c
+			lex.toklen = lex.toklen + 1
+
+			c = getcc(lex)
+			if c == ">" {
+				lex.token[lex.toklen] = c
+				lex.toklen = lex.toklen + 1
+			} else {
+				putcc(lex, c)
+			}
+		} else if c == eof {
+			return false
+		} else {
+			lex.token[lex.toklen] = c
+			lex.toklen = lex.toklen + 1
+		}
+	}
+
+	lex.token[lex.toklen] = "\x0"
+	return true
+}
+
+
+
+
+public func main() -> Int {
+	printf("text_file example\n")
+
+	var lexer: Lexer
+	init(&lexer)
+	lexer.fd = fopen(filename, "r")
+	while true {
+		let rc: Bool = gettok(&lexer)
+		if not rc {
+			printf(">> END.\n")
+			break
+		}
+		printf("TOKEN = '%s'\n", &lexer.token)
+	}
+
+	fclose(lexer.fd)
+	return 0
+}
+
