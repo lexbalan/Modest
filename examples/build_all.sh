8b36c100c49cce1642de63f8a70a2c452def7ff1
Alex Balan <outsiders.b@gmail.com>
2025-05-22 17:47:29 +0300

    1

--- examples/build_all.sh
+++ examples/build_all.sh
@@ -1,18 +1,18 @@
-cd 1.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd 3.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd 4.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd 5.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd 6.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd 7.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd 8.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd 9.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd 10.* && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd stmt_if && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd stmt_while && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. &&\
-cd demo1 && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
-cd bubble_sort && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. #&& \
-cd web && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. #&& \
-#cd m328p_blink && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd ..
-cd sha256 && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd ..
-cd table && echo "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd ..
+cd 1.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd 3.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd 4.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd 5.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd 6.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd 7.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd 8.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd 9.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd 10.* && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd stmt_if && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd stmt_while && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. &&\
+cd demo1 && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. && \
+cd bubble_sort && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. #&& \
+cd web && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd .. #&& \
+#cd m328p_blink && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd ..
+cd sha256 && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd ..
+cd table && echo -c "\nBUILD:" "$PWD" && make -j7 && make -j7 C && make -j7 CM && cd ..
 
