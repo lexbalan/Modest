0157650bf52ce6b813757dbed32b8125659a6d78
Alex Balan <outsiders.b@gmail.com>
2025-10-18 22:43:26 +0300

    + _CHR<X>() macro

--- tests/11.unicode/out/c/main.h
+++ tests/11.unicode/out/c/main.h
@@ -19,12 +19,15 @@ typedef uint16_t char16_t;
 typedef uint32_t char32_t;
 #endif
 #define __STR_UNICODE__
-#define __STR8(x) x
+#define __STR8(x)  x
 #define __STR16(x) u##x
 #define __STR32(x) U##x
-#define _STR8(x) __STR8(x)
+#define _STR8(x)  __STR8(x)
 #define _STR16(x) __STR16(x)
 #define _STR32(x) __STR32(x)
+#define _CHR8(x)  (__STR8(x)[0])
+#define _CHR16(x) (__STR16(x)[0])
+#define _CHR32(x) (__STR32(x)[0])
 #endif /* __STR_UNICODE__ */
 
 #include "console.h"
