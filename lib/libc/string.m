720e2db07021fb1c812e517e291e184145a6e1af
Alex Balan <outsiders.b@gmail.com>
2025-11-03 20:26:02 +0200

    1

--- lib/libc/string.m
+++ lib/libc/string.m
@@ -11,13 +11,17 @@ public func memset (mem: Ptr, c: Int, n: SizeT) -> @unused Ptr
 public func memcpy (dst: Ptr, src: Ptr, len: SizeT) -> @unused Ptr
 public func memmove (dst: Ptr, src: Ptr, n: SizeT) -> @unused Ptr
 public func memcmp (p0: Ptr, p1: Ptr, num: SizeT) -> Int
-public func strncmp (s1: *[]ConstChar, s2: *[]ConstChar, n: SizeT) -> Int
-public func strcmp (s1: *[]ConstChar, s2: *[]ConstChar) -> Int
-public func strcpy (dst: *[]Char, src: *[]ConstChar) -> @unused *[]Char
 public func strlen (s: *[]ConstChar) -> SizeT
+public func strcmp (s1: *[]ConstChar, s2: *[]ConstChar) -> Int
+public func strncmp (s1: *[]ConstChar, s2: *[]ConstChar, n: SizeT) -> Int
+public func strcpy (dst: *[]Char, src: *[]ConstChar) -> @unused *[]Char
+public func strncpy (dst: *[]Char, src: *[]ConstChar, n: SizeT) -> @unused *[]Char
 public func strcat (s1: *[]Char, s2: *[]ConstChar) -> *[]Char
 public func strncat (s1: *[]Char, s2: *[]ConstChar, n: SizeT) -> *[]Char
 public func strerror (error: Int) -> *[]Char
 
 public func strcspn (str1: *Str8, str2: *Str8) -> SizeT
+
+
+
 
