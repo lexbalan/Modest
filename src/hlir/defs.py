ee589e6a44301722409e687e6dae5f36c3b4d045
Alex Balan <outsiders.b@gmail.com>
2025-11-09 16:42:34 +0200

    1

--- src/hlir/defs.py
+++ src/hlir/defs.py
@@ -3,24 +3,6 @@ from .types import *
 
 
 
-unit_id = Id('Unit')
-unit_id.c = 'void'
-unit_id.llvm = 'void'
-typeUnit = TypeSimple(0, HLIR_TYPE_KIND_UNIT, unit_id, UNIT_OPS)
-
-
-bool_id = Id('Bool')
-bool_id.c = 'bool'
-bool_id.llvm = 'Bool'
-typeBool = TypeSimple(8, HLIR_TYPE_KIND_BOOL, bool_id, BOOL_OPS)
-
-
-#def type_bad_create(ti=None):
-#	nt = TypeSimple(width=0, kind=HLIR_TYPE_KIND_BAD, id=Id(None), ops=[], ti=ti)
-#	nt.generic = True
-#	return nt
-
-
 def type_number_create(width=0, ti=None):
 	nt = TypeSimple(width, HLIR_TYPE_KIND_NUMBER, Id(None), NUM_OPS, ti)
 	nt.generic = True
@@ -47,14 +29,6 @@ def type_word_create(width, ti=None):
 	return TypeSimple(width, HLIR_TYPE_KIND_WORD, id, WORD_OPS, ti)
 
 
-typeWord8 = type_word_create(width=8)
-typeWord16 = type_word_create(width=16)
-typeWord32 = type_word_create(width=32)
-typeWord64 = type_word_create(width=64)
-typeWord128 = type_word_create(width=128)
-typeWord256 = type_word_create(width=256)
-
-
 def type_int_create(width, ti=None):
 	width = align_bits_up(width)
 	id = Id('Int%d' % width)
@@ -65,15 +39,6 @@ def type_int_create(width, ti=None):
 	id.llvm = 'Int%d' % width
 	nt = TypeSimple(width, HLIR_TYPE_KIND_INT, id, INT_OPS, ti)
 	return nt
-
-
-
-typeInt8 = type_int_create(width=8)
-typeInt16 = type_int_create(width=16)
-typeInt32 = type_int_create(width=32)
-typeInt64 = type_int_create(width=64)
-typeInt128 = type_int_create(width=128)
-typeInt256 = type_int_create(width=256)
 
 
 def type_nat_create(width, ti=None):
@@ -88,16 +53,6 @@ def type_nat_create(width, ti=None):
 	return nt
 
 
-
-typeNat8 = type_nat_create(width=8)
-typeNat16 = type_nat_create(width=16)
-typeNat32 = type_nat_create(width=32)
-typeNat64 = type_nat_create(width=64)
-typeNat128 = type_nat_create(width=128)
-typeNat256 = type_nat_create(width=256)
-
-
-
 def type_char_create(width, ti=None):
 	width = align_bits_up(width)
 	id = Id('Char%d' % width)
@@ -107,12 +62,6 @@ def type_char_create(width, ti=None):
 		id.c = 'char%d_t' % width
 	id.llvm = 'Char%d' % width
 	return TypeSimple(width, HLIR_TYPE_KIND_CHAR, id, CHAR_OPS, ti)
-
-
-
-typeChar8 = type_char_create(width=8)
-typeChar16 = type_char_create(width=16)
-typeChar32 = type_char_create(width=32)
 
 
 def type_float_create(width, ti=None):
@@ -126,6 +75,42 @@ def type_float_create(width, ti=None):
 	nt = TypeSimple(width, HLIR_TYPE_KIND_FLOAT, id, FLOAT_OPS, ti)
 	return nt
 
+
+
+unit_id = Id('Unit')
+unit_id.c = 'void'
+unit_id.llvm = 'void'
+typeUnit = TypeSimple(0, HLIR_TYPE_KIND_UNIT, unit_id, UNIT_OPS)
+
+bool_id = Id('Bool')
+bool_id.c = 'bool'
+bool_id.llvm = 'Bool'
+typeBool = TypeSimple(8, HLIR_TYPE_KIND_BOOL, bool_id, BOOL_OPS)
+
+typeWord8 = type_word_create(width=8)
+typeWord16 = type_word_create(width=16)
+typeWord32 = type_word_create(width=32)
+typeWord64 = type_word_create(width=64)
+typeWord128 = type_word_create(width=128)
+typeWord256 = type_word_create(width=256)
+
+typeInt8 = type_int_create(width=8)
+typeInt16 = type_int_create(width=16)
+typeInt32 = type_int_create(width=32)
+typeInt64 = type_int_create(width=64)
+typeInt128 = type_int_create(width=128)
+typeInt256 = type_int_create(width=256)
+
+typeNat8 = type_nat_create(width=8)
+typeNat16 = type_nat_create(width=16)
+typeNat32 = type_nat_create(width=32)
+typeNat64 = type_nat_create(width=64)
+typeNat128 = type_nat_create(width=128)
+typeNat256 = type_nat_create(width=256)
+
+typeChar8 = type_char_create(width=8)
+typeChar16 = type_char_create(width=16)
+typeChar32 = type_char_create(width=32)
 
 typeFloat32 = type_float_create(width=32)
 typeFloat64 = type_float_create(width=64)
