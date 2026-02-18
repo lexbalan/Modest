b3087f649e9a79fb52a5e7ad26863bef0bd0d907
Alex Balan <outsiders.b@gmail.com>
2026-02-18 22:52:14 +0200

    1

--- src/hlir/types.py
+++ src/hlir/types.py
@@ -647,11 +647,11 @@ class Type(Entity):
 				nt.layout = 'union'
 			elif k == 'record':
 				nt.layout = 'record'
+			elif k == 'exact':
+				nt.layout = 'exact'
 			elif k == 'packed':
 				nt.layout = 'packed'
 				nt.att.append(k)
-			elif k == 'exact':
-				nt.layout = 'exact'
 
 
 			if k == 'zarray':
