cb05a7834076d687a90543af4629cde8e553aaf6
Alex Balan <outsiders.b@gmail.com>
2026-06-22 20:01:02 +0300

    1

--- tests/or/src/main.m
+++ tests/or/src/main.m
@@ -8,11 +8,11 @@ const errorNone = Error 0
 const errorSome = Error 1
 
 func main () -> Int or Error {
-  let x = errorSome
-  if x == errorNone {
-	printf("No error\n")
-  } else {
-	printf("Error occurred\n")
-  }
+	let x = errorSome
+	if x == errorNone {
+		printf("No error\n")
+	} else {
+		printf("Error occurred\n")
+	}
 	return 0
 }
