137c18a3cfb37ca6fc2148ace25910e173da9906
Alex Balan <outsiders.b@gmail.com>
2026-08-26 23:19:24 +0300

    1

--- examples/0/out/c/main.c
+++ examples/0/out/c/main.c
@@ -3,14 +3,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
-static int32_t a = 10;
-static int32_t b = 6;
-static int32_t c = 4;
 
 int main(void) {
 	printf("Hello World!\n");
-	const int32_t x = a - (b - c);
-	printf("a - (b - c) = %d\n", x);
 	return 0;
 }
 
